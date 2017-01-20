590bd43bf1f076bf6cc3d612a9ab0aeea14740c9
Elijah Mirecki <elimirks@gmail.com>
2017-01-20 14:34:14 -0500

    Added i3 configs

---
+++ i3/i3exit.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+lock() {
+    ~/.i3/i3lock-fancy/lock
+}
+
+case "$1" in
+    lock)
+        lock
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    suspend)
+				#lock&
+				#sleep 1
+        sudo pm-suspend
+				sleep 10
+				xmodmap /home/elijah/.Xmodmap
+        ;;
+    hibernate)
+        lock && systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+        exit 2
+esac
+
+exit 0
+
