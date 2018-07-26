123056be2acba388b7fe86303fa3dc80bd28ed37
Elijah Mirecki <elimirks@gmail.com>
2018-07-26 12:06:10 -0400

    Cleanup Emac

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -14,6 +14,10 @@
 
 (org-babel-load-file "~/.emacs.d/README.org")
 
+;; For environment specific things, such as super secret passwords
+(when (file-exists-p "~/.emacs.d/user.org")
+  (org-babel-load-file "~/.emacs.d/user.org"))
+
 ;; NTS: don't put anything except custom set stuff in this config
 
 (custom-set-variables
@@ -25,7 +29,7 @@
  '(org-agenda-files (quote ("~/Dropbox/Notes/everything.org")))
  '(package-selected-packages
    (quote
-    (edit-indirect smart-semicolon smartparens evil-smartparens speed-type multi-term yaml-mode pdf-tools ac-php company-php 4clojure quickrun php-extras php-mode doc-view-mode gnus-desktop-notify copy-as-format slack flow-minor-mode arduino-mode omnisharp csharp-mode love-minor-mode eslint-fix general web-mode tide ggtags helm-rtags rtags gtags markdown-mode org-bullets nlinum-relative evil-org evil-magit which-key magit ido-complete-space-or-hyphen smex ido-ubiquitous powerline-evil powerline evil-surround evil-leader evil rainbow-delimiters use-package)))
+    (edit-indirect smart-semicolon smartparens evil-smartparens speed-type multi-term yaml-mode pdf-tools ac-php company-php 4clojure quickrun php-extras php-mode doc-view-mode gnus-desktop-notify copy-as-format slack flow-minor-mode arduino-mode omnisharp csharp-mode love-minor-mode eslint-fix general web-mode tide ggtags helm-rtags rtags gtags markdown-mode org-bullets nlinum-relative evil-org evil-magit which-key magit powerline-evil powerline evil-surround evil-leader evil rainbow-delimiters use-package)))
  '(send-mail-function (quote smtpmail-send-it))
  '(zoom-size (quote (0.8 . 0.8))))
 (custom-set-faces
--- emacs.d/lisp/user.dist.el
+++
@@ -1,2 +0,0 @@
-;; Put user / PC specific things in here, such as supa-secret-passwords (rename to user.el)
-(provide 'user)
