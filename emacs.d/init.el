0156dca44ed55f36fdb8f7d5a138bcecb031f1b2
Elijah Mirecki <elimirks@gmail.com>
2020-02-03 13:52:00 +0000

    Fix some issues with using terminal emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -37,10 +37,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-preview-common ((t (:foreground unspecified :background "#199919991999"))))
- '(company-scrollbar-bg ((t (:background "#199919991999"))))
- '(company-scrollbar-fg ((t (:background "#0ccc0ccc0ccc"))))
- '(company-tooltip ((t (:inherit default :background "#051e051e051e"))))
+ '(company-preview-common ((t (:foreground unspecified :background "#191919"))))
+ '(company-scrollbar-bg ((t (:background "#191919"))))
+ '(company-scrollbar-fg ((t (:background "#0c0c0c"))))
+ '(company-tooltip ((t (:inherit default :background "#050505"))))
  '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
  '(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
  '(linum ((t (:background "color-16" :foreground "#9fc59f"))))
