8e07ef15127659b5d443101d3169562c4f9892b4
Elijah Mirecki <elimirks@gmail.com>
2020-02-04 09:06:41 -0500

    Add Scala mode supports via Metals

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -25,6 +25,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c9e858c340109fc5f722e40ffd1c59e437ebfb858793fdf70e76732e1df8a6c5" default)))
  '(flycheck-javascript-flow-args nil)
  '(org-agenda-files (quote ("~/Dropbox/Notes/everything.org")))
  '(package-selected-packages
