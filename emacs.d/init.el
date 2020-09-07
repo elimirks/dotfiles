beba39bfae8da497547dea18440033884997abb8
Elijah Mirecki <elimirks@gmail.com>
2020-09-07 18:51:32 -0400

    Update emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -35,24 +35,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("c9e858c340109fc5f722e40ffd1c59e437ebfb858793fdf70e76732e1df8a6c5" default)))
+   '("c9e858c340109fc5f722e40ffd1c59e437ebfb858793fdf70e76732e1df8a6c5" default))
  '(flycheck-javascript-flow-args nil)
- '(org-agenda-files (quote ("~/Dropbox/Notes/everything.org")))
+ '(org-agenda-files '("~/Dropbox/Notes/everything.org"))
  '(package-selected-packages
-   (quote
-    (flycheck-rust rust-mode google-this jedi smart-tabs-mode objc-font-lock ox-mdx-deck swift-mode evil-lion groovy-mode gradle-mode lsp-intelij lsp-intellij kotlin-mode lsp-ui lsp-javascript-typescript eglot lsp-php fill-column-indicator helm-ag unicode-fonts company-jedi bison-mode emms-setup xkcd restclient-company company-restclient restclient-helm restclient general edit-indirect smart-semicolon smartparens evil-smartparens speed-type multi-term yaml-mode pdf-tools ac-php company-php 4clojure quickrun php-extras php-mode doc-view-mode gnus-desktop-notify copy-as-format slack flow-minor-mode arduino-mode omnisharp love-minor-mode eslint-fix web-mode tide ggtags helm-rtags rtags gtags markdown-mode org-bullets nlinum-relative evil-org evil-magit which-key magit powerline-evil powerline evil-surround evil-leader evil rainbow-delimiters use-package)))
- '(send-mail-function (quote smtpmail-send-it))
- '(zoom-size (quote (0.8 . 0.8))))
+   '(psc-ide purescript-mode flycheck-rust rust-mode google-this jedi smart-tabs-mode objc-font-lock ox-mdx-deck swift-mode evil-lion groovy-mode gradle-mode lsp-intelij lsp-intellij kotlin-mode lsp-ui lsp-javascript-typescript eglot lsp-php fill-column-indicator helm-ag unicode-fonts company-jedi bison-mode emms-setup xkcd restclient-company company-restclient restclient-helm restclient general edit-indirect smart-semicolon smartparens evil-smartparens speed-type multi-term yaml-mode pdf-tools ac-php company-php 4clojure quickrun php-extras php-mode doc-view-mode gnus-desktop-notify copy-as-format slack flow-minor-mode arduino-mode omnisharp love-minor-mode eslint-fix web-mode tide ggtags helm-rtags rtags gtags markdown-mode nlinum-relative evil-org evil-magit which-key magit powerline-evil powerline evil-surround evil-leader evil rainbow-delimiters use-package))
+ '(send-mail-function 'smtpmail-send-it)
+ '(zoom-size '(0.8 . 0.8)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-preview-common ((t (:foreground unspecified :background "#191919"))))
- '(company-scrollbar-bg ((t (:background "#191919"))))
- '(company-scrollbar-fg ((t (:background "#0c0c0c"))))
- '(company-tooltip ((t (:inherit default :background "#050505"))))
+ '(company-preview-common ((t (:foreground unspecified :background "#199919991999"))))
+ '(company-scrollbar-bg ((t (:background "#199919991999"))))
+ '(company-scrollbar-fg ((t (:background "#0ccc0ccc0ccc"))))
+ '(company-tooltip ((t (:inherit default :background "#051e051e051e"))))
  '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
  '(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
  '(linum ((t (:background "color-16" :foreground "#9fc59f"))))
