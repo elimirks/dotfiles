d8df9da870cb2caa9ce23909164b0d7433f865e3
Elijah Mirecki <elimirks@gmail.com>
2021-05-06 10:13:52 -0400

    Add count option to all_uniq

--- bin/all_uniq.py
+++ bin/all_uniq.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
+import argparse
 import fileinput
+import sys
 
-lines = set()
+def only_uniq():
+    lines = set()
+    
+    for line in sys.stdin:
+        if not line in lines:
+            lines.add(line)
+            print(line.rstrip('\n'))
 
-for line in fileinput.input():
-    if line in lines:
-        continue
+def uniq_counts():
+    lines = dict()
 
-    lines.add(line)
-    print(line.rstrip('\n'))
+    for line in sys.stdin:
+        if line in lines:
+            lines[line] += 1
+        else:
+            lines[line] = 1
+
+    for line,count in lines.items():
+        print('{:>10} {}'.format(count, line.rstrip('\n')))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Count unique lines')
+    parser.add_argument('-c', '--count', dest='count', action='store_const',
+                        const=True, default=False,
+                        help='Display counts of duplicate lines')
+
+    args = parser.parse_args()
+
+    if args.count:
+        uniq_counts()
+    else:
+        only_uniq()
+    
