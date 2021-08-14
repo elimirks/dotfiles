9184d7e8920e4b1084d68ea50968be44dc2463a7
Elijah Mirecki <elimirks@gmail.com>
2021-08-14 18:13:54 -0400

    Add org roam

--- emacs.d/config/org.el
+++ emacs.d/config/org.el
@@ -98,3 +98,21 @@
       (make-list 3 (concat
                     "pdflatex -shell-escape -interaction nonstopmode"
                     " -output-directory %o %f")))
+
+(use-package org-roam
+      :ensure t
+      :init
+      (setq org-roam-v2-ack t)
+      :custom
+      (org-roam-directory (file-truename "~/Dropbox/roam/"))
+      :bind (("C-c n l" . org-roam-buffer-toggle)
+             ("C-c n f" . org-roam-node-find)
+             ("C-c n g" . org-roam-graph)
+             ("C-c n i" . org-roam-node-insert)
+             ("C-c n c" . org-roam-capture)
+             ;; Dailies
+             ("C-c n j" . org-roam-dailies-capture-today))
+      :config
+      (org-roam-setup)
+      ;; If using org-roam-protocol
+      (require 'org-roam-protocol))
