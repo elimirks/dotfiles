062f3ac1655c64ac01de63feaa7168797b1b1f6d
Josh Wolfe <joshuafwolfe@gmail.com>
2025-03-23 20:35:26 -0400

    Move to snacks picker

--- user.nvim/init.lua
+++ user.nvim/init.lua
@@ -39,7 +39,7 @@ function user_config()
     vim.o.wrap = true
     vim.o.shortmess = "ltToOCFm"
 
-    local fzf = require('fzf-lua')
+    local sp = require('snacks.picker')
 
     require('legendary').keymaps({
         { '<Space>' , '<nop>' },
@@ -58,18 +58,16 @@ function user_config()
         -- Format
         { '<leader>F', '<cmd>Neoformat<cr>', mode = { 'n', 'v' } },
 
-        -- fzf
-        --{ '<leader>f', function() fzf_find_file(vim.loop.cwd()) end },
-        --{ '<leader>e', function() fzf_find_file(vim.fs.dirname(vim.api.nvim_buf_get_name(0))) end},
-        { '<leader>f', fzf.files },
-        { '<leader>e', function() fzf_find_file(get_buf_dir()) end },
-        { '<leader>m', fzf.buffers },
-        { '<leader>g', fzf.live_grep },
-        { '<leader>r', fzf.resume },
-        { '<M-x>', fzf.commands },
+        -- Pickers
+        { '<leader>f', function() sp.git_files({ untracked = true }) end },
+        { '<leader>e', snacks_find_file },
+        { '<leader>m', sp.buffers },
+        { '<leader>g', sp.grep() },
+        { '<leader>r', sp.resume },
+        { '<M-x>', sp.commands },
 
         -- Help
-        { '<C-h>f', fzf.help_tags },
+        { '<C-h>f', sp.help},
         { '<C-h>k', '<cmd>Legendary<cr>' },
 
         -- Neogit
@@ -102,18 +100,18 @@ end
 
 function user_on_lsp_attach(client, bufnr)
     local map_opts = { buffer = true }
-    local fzf = require('fzf-lua')
+    local sp = require('snacks.picker')
 
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     require('legendary').keymaps({
-        { 'gd', fzf.lsp_definitions, opts = map_opts },
+        { 'gd', sp.lsp_definitions, opts = map_opts },
         { 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts = map_opts },
         { 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts = map_opts },
         { 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts = map_opts },
-        { 'gi', fzf.lsp_implementations, opts = map_opts },
+        { 'gi', sp.lsp_implementations, opts = map_opts },
         { '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts = map_opts },
-        { 'gr', fzf.lsp_references, opts = map_opts },
+        { 'gr', sp.lsp_references, opts = map_opts },
         { '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts = map_opts },
         { ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts = map_opts },
         { '<leader>F', '<cmd>lua vim.lsp.buf.formatting()<cr>', opts = map_opts },
