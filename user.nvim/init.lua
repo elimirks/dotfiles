403df80c2852de74071a35672ed98d58cba1a082
Elijah Mirecki <elimirks@gmail.com>
2025-09-09 14:14:49 -0300

    Update dap layout

--- user.nvim/init.lua
+++ user.nvim/init.lua
@@ -229,29 +229,33 @@ function user_config()
         layouts = { {
             elements = { {
                 id = "scopes",
-                size = 0.25
+                size = 0.25,
             }, {
                 id = "breakpoints",
-                size = 0.25
+                size = 0.25,
             }, {
                 id = "stacks",
-                size = 0.25
+                size = 0.25,
             }, {
                 id = "watches",
-                size = 0.25
+                size = 0.25,
             } },
             position = "left",
-            size = 40
+            size = 0.2,
+        }, {
+            elements = { {
+                id = "console",
+                size = 100,
+            } },
+            position = "right",
+            size = 0.3,
         }, {
             elements = { {
                 id = "repl",
-                size = 0.5
-            }, {
-                id = "console",
-                size = 0.5
-            } },
+                size = 1,
+            }, },
             position = "bottom",
-            size = 10
+            size = 0.2,
         } },
     })
 
@@ -285,7 +289,11 @@ function user_config()
         },
     }
 
+    dap.listeners.before.event_initialized["dapui_config"] = function()
+        dapui.open();
+    end
     dap.listeners.before.event_terminated["dapui_config"] = function()
+        dap.terminate({ all = true })
     end
     dap.listeners.before.event_exited["dapui_config"] = function()
     end
